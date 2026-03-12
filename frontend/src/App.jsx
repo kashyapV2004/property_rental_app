110fdcd8dfb7b20789ac14d3437cc669096d0079
Vinit kashyap <vinitkumar10062004@gmail.com>
2026-03-13 02:17:38 +0530

    changes in UserContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Nav from "./component/Nav.jsx";
 import Footer from "./component/footer.jsx";
@@ -8,51 +8,51 @@ import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import Listings from "./pages/Listings.jsx";
 import Show from "./pages/Show.jsx";
+import Edit from "./pages/Edit.jsx";
 import UserContext from "./context/UserContext.jsx";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Edit from "./pages/Edit.jsx";
+import { LoaderProvider } from "./context/LoaderContext.jsx";
 
 function App() {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
 
-    useEffect(() => {
-      axios
-        .get("http://localhost:8080/current-user", {
-          withCredentials: true,
-        })
-        .then((res) => {
-          setCurrentUser(res.data);
-          setLoading(false);
-        })
-        .catch((err) => {
-          console.log(err);
-          setLoading(false);
-        });
-    }, []);
-
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/current-user", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setCurrentUser(res.data);
+      })
+      .catch((err) => {
+        toast.error(err.message);
+        console.log(err);
+      });
+  }, []);
 
   return (
     <>
-      <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-        <Nav />
-        <ToastContainer/>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/listings" element={<Listings />} />
-            <Route path="/listings/new" element={<AddListing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/listings/:id" element={<Show />} />
-            <Route path="/listings/:id/edit" element={<Edit/>}/>
-          </Routes>
-        </div>
-        <Footer />
-      </UserContext.Provider>
+      <LoaderProvider>
+        <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+          <Nav />
+          <ToastContainer />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/listings" element={<Listings />} />
+              <Route path="/listings/new" element={<AddListing />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/listings/:id" element={<Show />} />
+              <Route path="/listings/:id/edit" element={<Edit />} />
+            </Routes>
+          </div>
+          <Footer />
+        </UserContext.Provider>
+      </LoaderProvider>
     </>
   );
 }
