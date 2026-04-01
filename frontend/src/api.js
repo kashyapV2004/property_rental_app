ab16a588d543898d909bd3186e3a27ddf040b9c0
Vinit kashyap <vinitkumar10062004@gmail.com>
2026-04-02 01:02:06 +0530

    change env variables

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: (import.meta.env.NODE_ENV === "production") ? import.meta.env.VITE_API_URL : "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
