05d59df632a02212f937a702c8d52d86bd464f12
Vinit kashyap <vinitkumar10062004@gmail.com>
2026-04-03 21:02:44 +0530

    added more filteres

--- frontend/src/component/Filters.jsx
+++ frontend/src/component/Filters.jsx
@@ -1,79 +1,59 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Filter() {
+  const navigate = useNavigate();
 
-    return (
-      <div id="filters">
-        <div className="filter">
+  const filters = [
+    { label: "Trending", icon: "fa-solid fa-fire", value: "trending" },
+    { label: "Rooms", icon: "fa-solid fa-bed", value: "room" },
+    {
+      label: "Iconic cities",
+      icon: "fa-solid fa-mountain-city",
+      value: "city",
+    },
+    { label: "Mountains", icon: "fa-solid fa-mountain", value: "mountain" },
+    { label: "Castles", icon: "fa-brands fa-fort-awesome", value: "castle" },
+    {
+      label: "Amazing Pools",
+      icon: "fa-solid fa-person-swimming",
+      value: "pool",
+    },
+    { label: "Camping", icon: "fa-solid fa-campground", value: "camping" },
+    { label: "Farms", icon: "fa-solid fa-cow", value: "farm" },
+    { label: "Arctic", icon: "fa-regular fa-snowflake", value: "snow" },
+  ];
+
+  const handleClick = (search) => {
+    navigate(`/listings?search=${search}`);
+  };
+  return (
+    <div id="filters">
+      {filters.map((item, index) => (
+        <div
+          key={index}
+          onClick={() => handleClick(item.value)}
+          className="filter"
+        >
           <div>
-            <i className="fa-solid fa-fire"></i>
+            <i className={item.icon}></i>
           </div>
-          <p>Trending</p>
+          <p>{item.label}</p>
         </div>
-        <div className="filter">
-          <div>
-            <i className="fa-solid fa-bed"></i>
-          </div>
-          <p>Rooms</p>
-        </div>
-        <div className="filter">
-          <div>
-            <i className="fa-solid fa-mountain-city"></i>
-          </div>
-          <p>Iconic Cities</p>
-        </div>
-        <div className="filter">
-          <div>
-            <i className="fa-solid fa-mountain"></i>
-          </div>
-          <p>Mountains</p>
-        </div>
-        <div className="filter">
-          <div>
-            <i className="fa-brands fa-fort-awesome"></i>
-          </div>
-          <p>Castles</p>
-        </div>
-        <div className="filter">
-          <div>
-            <i className="fa-solid fa-person-swimming"></i>
-          </div>
-          <p>Amazing Pools</p>
-        </div>
-        <div className="filter">
-          <div>
-            <i className="fa-solid fa-campground"></i>
-          </div>
-          <p>Camping</p>
-        </div>
-        <div className="filter">
-          <div>
-            <i className="fa-solid fa-cow"></i>
-          </div>
-          <p>Farms</p>
-        </div>
-        <div className="filter">
-          <div>
-            <i className="fa-regular fa-snowflake"></i>
-          </div>
-          <p>Arctic</p>
-        </div>
-        <div className="tax-toggle">
-          <div className="form-check-reverse form-switch">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckChecked"
-            />
-            <label
-              className="form-check-label"
-              htmlFor="flexSwitchCheckChecked"
-            >
-              Display total after taxes
-            </label>
-          </div>
+      ))}
+      <div className="tax-toggle">
+        <div className="form-check-reverse form-switch">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckChecked"
+          />
+          <label className="form-check-label" htmlFor="flexSwitchCheckChecked">
+            Display total after taxes
+          </label>
         </div>
       </div>
-    );
+    </div>
+  );
 }
